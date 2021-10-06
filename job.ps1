a67b3c498142977b7263c2781a784707e072c7a3
Roy Ashbrook <rashbrook@patriottrans.com>
2021-10-06 15:50:33 -0400

    update sql import to limit cmd, add examples

--- job.ps1
+++ job.ps1
@@ -11,8 +11,7 @@ function main {
     Import-Module Add-PrefixForLogging
     Import-Module Clear-Files
     Import-Module Send-FileViaEmail
-    Import-Module ImportExcel
-    Import-Module SqlServer
+    Import-Module SqlServer -Cmdlet "Invoke-Sqlcmd"
 
     #get config, set primary output file name
     $cfg = Get-Content settings.json -Raw | ConvertFrom-Json
@@ -22,17 +21,50 @@ function main {
     "`n`n"
     l "Start"
     l "Get Data"
-        $dt = Invoke-Sqlcmd -QueryTimeout 1800 -ConnectionString $cfg.cs -InputFile $cfg.sql -OutputAs DataTables
-        if (($dt | Measure-Object).count -gt 0) {
-            $dt |
-            Select-Object $dt.Columns.ColumnName |
-            Export-Excel $file -TableStyle Medium6 -AutoSize -NoNumberConversion *
-        }
-        else {
-            "No data available"
-        }
-    l "Use Data"
+
+    # output to log only
+    # Invoke-Sqlcmd -QueryTimeout 1800 -ConnectionString $cfg.cs -InputFile $cfg.sql |
+    #     Select-Object * -ExcludeProperty RowError,RowState,Table,ItemArray,HasErrors |
+    #     Format-Table -Property * -AutoSize |
+    #     Out-String -Width 4096
+
+    $dt = Invoke-Sqlcmd -QueryTimeout 1800 -ConnectionString $cfg.cs -InputFile $cfg.sql -OutputAs DataTables
+    if (($dt | Measure-Object).count -gt 0) {
+
+        # csv
+        $dt |
+        Select-Object $dt.Columns.ColumnName |
+        Export-Csv $file -NoTypeInformation
+
+        # unquoted csv
+        # ConvertTo-Csv -NoTypeInformation |
+        # # if no header row allowed
+        # # Select-Object -Skip 1 |
+        # ForEach-Object{ "$($_ -replace '"', '')" } |
+        # Set-Content $file
+
+        # excel
+        # # Import-Module ImportExcel
+        # Export-Excel $file -TableStyle Medium6 -AutoSize -NoNumberConversion *
+
+        l "Use Data"
+
+        # send file vial email using msgraph
         Send-FileViaEmail $file $cfg -useGraph
+
+        #send file using ftp
+        # Import-Module WinSCP
+        # $securepass = ConvertTo-SecureString $cfg.ftp.pass -AsPlainText -Force
+        # $credential = New-Object pscredential ($cfg.ftp.user, $securepass)
+        # $sessionOption = New-WinSCPSessionOption -HostName $cfg.ftp.host -Credential $credential -Protocol Ftp -FtpSecure Explicit
+        # New-WinSCPSession -SessionOption $sessionOption
+        # Send-WinSCPItem $file
+        # Remove-WinSCPSession
+
+    }
+    else {
+        "No data available"
+    }
     l "Cleanup"    ; Clear-Files $cfg
     l "End"
     "`n`n"
